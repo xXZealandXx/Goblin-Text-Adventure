e73f14c9a67751c16d47502b1452d94fa523593f
xXZealandXx <102627057+xXZealandXx@users.noreply.github.com>
2023-03-01 19:24:11 -0800

    Add files via upload

---
+++ Goblin time.py
@@ -0,0 +1,61 @@
+import random
+def main():
+    print("Welcome to the Goblin School!")
+    print("You are a new student at the Goblin School.")
+    print("You are ready to start a new life in the school")
+    print("You get through your first classes quickly and now its time for lunch")
+    print("You are in the cafeteria.")
+    print("You see a goblin sitting at a table.")
+    print("You can either sit with the goblin or sit at a different table.")
+    print("What do you do?")
+    print("1. Sit with the goblin.")
+    print("2. Sit at a different table.")
+    choice = input("Enter 1 or 2: ")
+    if choice == "1":
+        print("The goblin says, 'Hi, my name is Bob.'")
+        print("You say, 'Hi, my name is...'")
+        name = input("What is your name? ")
+        print("You say, 'Hi, my name is "+name+ ".'")
+        print("Bob says, 'Nice to meet you, "+name+ ".'")
+        print("You and Bob become friends.")
+        print("You guys eatt to lunch together chatting it up about goblin food")
+        print("The bell rings and you go to class")
+        print("You and Bob are now in the classroom.")
+        print("The teacher says, 'Today we are going to learn about goblins.'")
+        print("The teacher says, 'What is a goblin?'")
+        print("a. A goblin is a small, green creature.")
+        print("b. A goblin is a large, blue creature.")
+        print("c. A goblin is a medium, purple cat.")
+        print("d. A goblin is a Sweet little munchkin.")
+        print("What do you say?")
+    else:
+        print("You have no friends you loser!.")
+        print("You eat alone thinking about what went wrong in your life adding to this point")
+        print(" You get up and go to class stumbling around the goblin halls")
+        
+        
+        choice == input("Enter a, b, c, or d:")
+        if choice == a:
+            print("The teacher says, 'That is correct.'")
+            print("Bob says, 'I knew that.'")
+            print("You say, 'screw you bob.'")
+            print("The teacher says, 'Good job, "+name+ " and Bob.'")
+        else:
+            print("The teacher says, 'That is wrong bozo.'")
+            print("Bob says, 'I knew that.'")
+            print("you say 'screw you bob!'")
+            print("Bob just disrespected you what are going to do about it?")
+            print("1. KILL BOB.")
+            print("2. Let It go.")
+            choice = input("Enter 1 or 2: ")
+            if choice == "1":
+                print("Bob never saw you coming...")
+                print("You emeidietly throw a right hook laying him out on the floor.")
+            else:
+                print("You and bob make up and end up being best friends.")
+                
+
+
+
+
+main()
